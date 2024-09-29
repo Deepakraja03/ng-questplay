fc4503256c007fb2cd4a63645f4953f6e2051134
Deepakraja03 <12306deepakraja@gmail.com>
2024-09-29 11:07:53 +0530

    completed the quest

--- campaigns/token-standards/building-erc-20/contracts/MintableDoubloon.sol
+++ campaigns/token-standards/building-erc-20/contracts/MintableDoubloon.sol
@@ -5,6 +5,23 @@ import "./Doubloon.sol";
 
 contract MintableDoubloon is Doubloon {
 
-    constructor(uint256 _supply) Doubloon(_supply) {}
+    address public owner;
+    constructor(uint256 _supply) Doubloon(_supply) {
+        owner = msg.sender;
+    }
 
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the contract owner");
+        _;
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner{
+        require(_to != address(0), "Invalid address");
+        require(_amount > 0, "Amount must be greater than zero");
+
+        totalSupply += _amount;
+        balanceOf[_to] += _amount;
+
+        emit Transfer(address(0), _to, _amount);
+    }
 }
